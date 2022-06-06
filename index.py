8810639c4fcf9eb3cbc54b9905f8fe7f6333c33b
ruuushhh <ruuushhh2001@gmail.com>
2022-06-07 00:38:43 +0530

    CRUD patient created

---
+++ index.py
@@ -0,0 +1,5 @@
+from fastapi import FastAPI
+from routes.patient import patient
+
+app = FastAPI()
+app.include_router(patient)
--- routes/patient.py
+++ routes/patient.py
@@ -1,29 +1,30 @@
-from fastapi import FastAPI
+from fastapi import APIRouter
 from config.db import conn
 from models.patient import Patient
 from schemas.patient import serializeDict, serializeList
+from bson import ObjectId
 
 
-app = FastAPI()
+patient = APIRouter()
 
 
-@app.post("/patient/", tags=["Patient"])
+@patient.post("/patient/", tags=["Patient"])
 async def insert_patient(patient: Patient):
     conn.patientdb.patient.insert_one(dict(Patient))
     return serializeList(conn.patientdb.patient.find())
 
 
-@app.get("/patient/", tags=["Patient"])
+@patient.get("/patient/", tags=["Patient"])
 async def get_all_patient():
     return serializeList(conn.patientdb.patient.find())
 
 
-@app.get("/patient/{id}", tags=["Patient"])
+@patient.get("/patient/{id}", tags=["Patient"])
 async def get_one_patient(id):
     return serializeDict(conn.patientdb.patient.find_one({"_id": ObjectId(id)}))
 
 
-@app.put("/patient/{id}", tags=["Patient"])
+@patient.put("/patient/{id}", tags=["Patient"])
 async def update_patient(id, patient: Patient):
     conn.patientdb.patient.find_one_and_update(
         {"_id": ObjectId(id)}, {"$set": dict(Patient)}
@@ -31,7 +32,7 @@ async def update_patient(id, patient: Patient):
     return serializeDict(conn.patientdb.patient.find_one({"_id": ObjectId(id)}))
 
 
-@app.delete("/patient/{id}", tags=["Patient"])
+@patient.delete("/patient/{id}", tags=["Patient"])
 async def delete_patient(id):
     return serializeDict(
         conn.patientdb.patient.find_one_and_delete({"_id": ObjectId(id)})
